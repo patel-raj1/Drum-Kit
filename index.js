3250403d1e67285738d6ec71a2032ade9ce39167
patel-raj1 <112147943+patel-raj1@users.noreply.github.com>
2024-01-11 15:23:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,80 @@
+//detecting button press
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i<numberOfDrumButtons ; i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+    function handleClick()
+    {
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+    }   
+}
+
+//detecting keybord press
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);//this show which key is pressed.
+    buttonAnimation(event.key);
+});
+
+function makeSound(key)
+{
+    switch (key) 
+    {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+    
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+    
+        case "l":
+            var kick_bass = new Audio("sounds/kick-bass.mp3");
+            kick_bass.play();
+            break;
+        
+        default:
+            console.log();
+            break;
+    }
+    
+}
+
+function buttonAnimation (currentKey)
+{
+    
+    var activeButton = document.querySelector("." + currentKey)
+    activeButton.classList.add("pressed"); //apllied animation css when key is pressed.
+
+    setTimeout(function()
+        {
+            activeButton.classList.remove("pressed");
+        }, 100);//remove the animation/pressed class after 0.1 sec
+}
+
+
